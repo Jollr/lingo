a732c96f388c7b95a0b189ca29e7885e7eaf7d68
Jolle Lont <jolle.lont@malengo.nl>
2015-04-16 14:22:38 +0200

    refactoring

--- gui.js
+++ gui.js
@@ -1,49 +1,58 @@
 var Gui = function() {	
+	var playField;
 	Dispatcher.Subscribe('started', function(message) {
-		var playField = new PlayField(message.numberOfCharacters, message.guessesPerWord);
+		playField = new PlayField(message.numberOfCharacters, message.guessesPerWord);
 		React.render( 
 			React.createElement(playField.GetComponent(), {}),
 			$('#guesses')[0]
 		);
-		/*React.render(
-			React.createElement(guessBox(message.numberOfCharacters), {}),
-			$('#guesses')[0]
-		);*/
+	});
+	
+	Dispatcher.Subscribe('guessUpdate', function(message) {
+		//playField.UpdateGuess(message.guess);
 	});
 };
 
 var PlayField = function(numberOfCharacters, guessesPerWord) {
 	var R = React.DOM;
-	var boxes = new Immutable.List();
+	var rows = new Immutable.List();
+	var rowIndex = 0;
 	
 	var letterBox = function() {
 		return R.div({className: 'charBox'});
 	};
 	
-	for (var n = 0; n < guessesPerWord*numberOfCharacters; n++) {
-		boxes = boxes.push(letterBox());
+	for (var n = 0; n < guessesPerWord; n++) {
+		var row = new Immutable.List();
+		
+		for (var m = 0; m < numberOfCharacters; m++) {
+			row = row.push(letterBox());
+		}
+		
+		rows = rows.push(row);
 	}
 	
-	var rows = [];
+	var rowDivs = [];
 	for (var n = 0; n < guessesPerWord; n++) {
-		var rowChildren = [];
-		for (var m = 0; m < numberOfCharacters; m++) {
-			rowChildren.push(boxes.get(n*numberOfCharacters + m));
-		}
-		rows.push(R.div( {children: rowChildren, className: 'guessRow row'}));
+		var rowChildren = rows.get(n).toArray();
+		rowDivs.push(R.div( {children: rowChildren, className: 'guessRow row'}));
 	}
 	
 	this.GetComponent = function () {
 		return React.createClass({ 
 			render: function() {
 				return R.div({
-					children: rows
+					children: rowDivs
 				});
 			}
 		});
 	};
 	
-	this.Set = function(wordt, at, ch) {
-		
-	};
+	/*this.UpdateGuess = function(guess) {
+		var row = rows.get(rowIndex);
+		var n = 0;
+		for (n = 0; n < guess.size; n++) {
+			row.get(n).html(guess.get(n));
+		}
+	};*/
 };
--- lingo.js
+++ lingo.js
@@ -1,12 +1,15 @@
 var Lingo = function() {
 	var words = Immutable.List.of('JOLLE', 'KEVIN', 'KNAKO');
+	var currentGuess = new Immutable.List();
 	var wordIndex = 0;
 	var numberOfCharacters = 5;
 	var guessesPerWord = 6;
 	var currentWord = words.get(wordIndex);
 	
+	
 	var onLetter = function(letter) {
-		console.log(letter);
+		currentGuess = currentGuess.push(letter);
+		Dispatcher.Publish('guessUpdate', {guess: currentGuess});
 	};
 	
 	this.Start = function() {
