01f7584ca925c7be82e33f178766f704cb65fc62
Jolle Lont <jolle.lont@malengo.nl>
2015-04-22 11:43:46 +0200

    allow f5 key

--- input.js
+++ input.js
@@ -3,8 +3,10 @@
 		if (e.keyCode >= 65 && e.keyCode <= 90) {
 			Dispatcher.Publish('letterTyped', { letter: String.fromCharCode(e.keyCode).toUpperCase() });
 			return false;
+		} else if (e.keyCode == 116) {
+			return true;
 		}
 		
-		//return false;
+		return false;
 	};
 };
